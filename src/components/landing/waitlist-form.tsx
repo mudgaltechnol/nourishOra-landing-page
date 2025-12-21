12f74504faa7aaf17e275e6c61fcc233e33f9666
lakshaymudgal <lakshaymudgal@countrydelight.in>
2025-12-21 22:50:18 +0530

    push to github

--- src/components/landing/waitlist-form.tsx
+++ src/components/landing/waitlist-form.tsx
@@ -187,6 +187,7 @@ export default function WaitlistForm() {
                       </FormItem>
                     )}
                   />
+                  
                   <FormField
                     control={form.control}
                     name="address"
